04c12b81ae39df97291e69149b7c68817c2c2e7f
Yaroslav Zozulya <my.dev.stories@gmail.com>
2022-07-05 21:58:49 +0300

    test husky

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,5 @@
 export const App = () => {
+  console.log('test log');
   return (
     <div
       style={{
@@ -7,7 +8,7 @@ export const App = () => {
         justifyContent: 'center',
         alignItems: 'center',
         fontSize: 40,
-        color: '#010101'
+        color: '#010101',
       }}
     >
       React homework template
